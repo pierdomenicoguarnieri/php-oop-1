1063c7a1cd9605877912919ed32c5274ffe90730
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-22 14:59:32 +0200

    fix: function settings

--- index.php
+++ index.php
@@ -5,12 +5,19 @@ class Movie{
   public $genre;
   public $length;
 
-  function __construct($_title, $_genre){
+  function __construct($_title, $_genre, $_length){
     $this->title = $_title;
     $this->genre = $_genre;
+    $this->length = $this->getTime($_length);
   }
 
-  public function setTime($_length){
-    $this->length = $_length/60;
+  public function getTime($_length){
+    return number_format($_length/60, 2, '.', "") . ' ore';
   }
 }
+
+$batman = new Movie('Batman', 'Action', 130);
+$spiderman = new Movie('Spiderman', 'Action', 200);
+
+var_dump($batman);
+var_dump($spiderman);
